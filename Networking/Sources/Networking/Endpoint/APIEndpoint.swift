a08b2b35b792ccc4caacc1db7c177738d6ce03be
Burakhan Ozay <burakozay10@gmail.com>
2026-02-04 17:31:06 +0100

    Created APIEndpoint protocol.

---
+++ Networking/Sources/Networking/Endpoint/APIEndpoint.swift
@@ -0,0 +1,25 @@
+//
+//  APIEndpoint.swift
+//  Networking
+//
+//  Created by BURAKHAN OZAY on 4.02.2026.
+//
+
+import Foundation
+
+public protocol APIEndpoint {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+
+    var headers: [String: String]? { get }
+    var queryItems: [String: String]? { get }
+    var body: Data? { get }
+}
+
+// TODO: - Clarify, in terms of generic usage, whether these default implementations are appropriate.
+public extension APIEndpoint {
+    var headers: [String: String]? { nil }
+    var queryItems: [String: String]? { nil }
+    var body: Data? { nil }
+}
