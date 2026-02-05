b098cdc715487a66313e84ec49b76266e662ff60
Burakhan Ozay <burakozay10@gmail.com>
2026-02-05 11:43:26 +0100

    - Created a new Protocol and an initilaizer for LocationsService

--- Places/Places/Scenes/Locations/Service/LocationsService.swift
+++ Places/Places/Scenes/Locations/Service/LocationsService.swift
@@ -9,25 +9,32 @@ import Foundation
 import Networking
 import NetworkModels
 
-final class LocationsService {
-    private let networkManager = NetworkManager()
+protocol LocationsServicing {
+    func fetchLocations() async throws -> LocationsResponse
+}
+
+final class LocationsService: LocationsServicing {
+
+    enum LocationsAPI: APIEndpoint {
+        case locations
+
+        var baseURL: String { "https://raw.githubusercontent.com" }
+        var path: String {
+            switch self {
+            case .locations:
+                return "/abnamrocoesd/assignment-ios/main/locations.json"
+            }
+        }
+        var method: HTTPMethod { .get }
+    }
+
+    private let networkManager: NetworkManaging
+
+    init(networkManager: NetworkManaging = NetworkManager()) {
+        self.networkManager = networkManager
+    }
 
     func fetchLocations() async throws -> LocationsResponse {
         return try await networkManager.request(LocationsAPI.locations)
     }
-}
-
-private enum LocationsAPI {
-    case locations
-}
-
-extension LocationsAPI: APIEndpoint {
-    var baseURL: String { "https://raw.githubusercontent.com" }
-    var path: String {
-        switch self {
-        case .locations:
-            return "/abnamrocoesd/assignment-ios/main/locations.json"
-        }
-    }
-    var method: HTTPMethod { .get }
 }
