016ad7d077532982e2aad49693d636e8dfb492e9
Burakhan Ozay <burakozay10@gmail.com>
2026-02-07 15:11:18 +0100

    Added public keywords, added module to Places target.

--- Storage/Sources/Storage/KeyValueStoring.swift
+++ Storage/Sources/Storage/KeyValueStoring.swift
@@ -5,7 +5,7 @@
 //  Created by BURAKHAN OZAY on 4.02.2026.
 //
 
-protocol KeyValueStoring {
+public protocol KeyValueStoring {
     func load<T: Decodable>(_ type: T.Type, forKey key: String) -> T?
     func save<T: Encodable>(_ value: T, forKey key: String)
 }
--- Storage/Sources/Storage/UserDefaultsStorage.swift
+++ Storage/Sources/Storage/UserDefaultsStorage.swift
@@ -7,20 +7,20 @@
 
 import Foundation
 
-final class UserDefaultsStorage: KeyValueStoring {
+public final class UserDefaultsStorage: KeyValueStoring {
 
     private let defaults: UserDefaults
 
-    init(defaults: UserDefaults = .standard) {
+    public init(defaults: UserDefaults = .standard) {
         self.defaults = defaults
     }
 
-    func load<T: Decodable>(_ type: T.Type, forKey key: String) -> T? {
+    public func load<T: Decodable>(_ type: T.Type, forKey key: String) -> T? {
         guard let data = defaults.data(forKey: key) else { return nil }
         return try? JSONDecoder().decode(type, from: data)
     }
 
-    func save<T: Encodable>(_ value: T, forKey key: String) {
+    public func save<T: Encodable>(_ value: T, forKey key: String) {
         guard let data = try? JSONEncoder().encode(value) else { return }
         defaults.set(data, forKey: key)
     }
